dbdd1e1bbfc8e0eaf394e173f534e6a3632ef348
a.s.janjanin <a.s.janjanin@student.rug.nl>
2018-09-25 16:39:40 +0200

    Small fix - mainActivity

--- app/src/main/java/allurosi/housebuddy/MainActivity.java
+++ app/src/main/java/allurosi/housebuddy/MainActivity.java
@@ -3,10 +3,8 @@ package allurosi.housebuddy;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 
 import allurosi.housebuddy.authentication.LogInActivity;
-import allurosi.housebuddy.todolist.ToDoListActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,11 +14,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Intent intent = new Intent(this, LogInActivity.class);
-        startActivity(intent);
-    }
-
-    public void buttonToDoList(View view) {
-        Intent intent = new Intent(this, ToDoListActivity.class);
         startActivity(intent);
     }
 
