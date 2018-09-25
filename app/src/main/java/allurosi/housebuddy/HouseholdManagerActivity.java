98ceb19dbc5e07bf2d05f2c15c627c89e3d25ab8
a.s.janjanin <a.s.janjanin@student.rug.nl>
2018-09-25 16:05:40 +0200

    Tweaking after the merge

--- app/src/main/java/allurosi/housebuddy/HouseholdManagerActivity.java
+++ app/src/main/java/allurosi/housebuddy/HouseholdManagerActivity.java
@@ -1,12 +1,15 @@
 package allurosi.housebuddy;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.View;
 import android.widget.Button;
 
 import com.google.firebase.auth.FirebaseAuth;
+
+import allurosi.housebuddy.todolist.ToDoListActivity;
 
 public class HouseholdManagerActivity extends Activity {
     private FirebaseAuth mAuth;
@@ -41,5 +44,16 @@ public class HouseholdManagerActivity extends Activity {
             }
         });
 
+        mToDoListBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                buttonToDoList();
+            }
+        });
+    }
+
+    public void buttonToDoList() {
+        Intent intent = new Intent(this, ToDoListActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/allurosi/housebuddy/MainActivity.java
+++ app/src/main/java/allurosi/housebuddy/MainActivity.java
@@ -3,10 +3,8 @@ package allurosi.housebuddy;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 
 import allurosi.housebuddy.authentication.LogInActivity;
-import allurosi.housebuddy.todolist.ToDoListActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,10 +16,4 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, LogInActivity.class);
         startActivity(intent);
     }
-
-    public void buttonToDoList(View view) {
-        Intent intent = new Intent(this, ToDoListActivity.class);
-        startActivity(intent);
-    }
-
 }
