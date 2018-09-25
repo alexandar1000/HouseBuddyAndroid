45e89d38593c022500244df52e4cc9b43fff54bd
a.s.janjanin <a.s.janjanin@student.rug.nl>
2018-09-25 18:29:55 +0200

    Google sign in works!

--- app/src/main/java/allurosi/housebuddy/authentication/LogInActivity.java
+++ app/src/main/java/allurosi/housebuddy/authentication/LogInActivity.java
@@ -11,12 +11,19 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 import allurosi.housebuddy.HouseholdManagerActivity;
 import allurosi.housebuddy.R;
@@ -25,15 +32,14 @@ import static android.content.ContentValues.TAG;
 
 public class LogInActivity extends AppCompatActivity {
 
+    private static final int RC_SIGN_IN = 1;
     private FirebaseAuth mAuth;
 
     private EditText mEmailInput;
     private EditText mPasswordInput;
 
-    private Button mLogInButton;
-    private Button mSignUpButton;
-    private SignInButton mGoogleSignIn;
-    private Button mFacebookSignIn;
+    private GoogleSignInClient mGoogleSignInClient;
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -44,10 +50,10 @@ public class LogInActivity extends AppCompatActivity {
 
         mEmailInput = (EditText) findViewById(R.id.emailInput);
         mPasswordInput = (EditText) findViewById(R.id.passwordInput);
-        mLogInButton = (Button) findViewById(R.id.logInButton);
-        mSignUpButton = (Button) findViewById(R.id.signUpButton);
-        mGoogleSignIn = (SignInButton) findViewById(R.id.googleSignIn);
-        mFacebookSignIn = (Button) findViewById(R.id.facebookSignIn);
+        Button mLogInButton = (Button) findViewById(R.id.logInButton);
+        Button mSignUpButton = (Button) findViewById(R.id.signUpButton);
+        SignInButton mGoogleSignIn = (SignInButton) findViewById(R.id.googleSignIn);
+        Button mFacebookSignIn = (Button) findViewById(R.id.facebookSignIn);
 
         mSignUpButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -62,6 +68,21 @@ public class LogInActivity extends AppCompatActivity {
                 logInUser();
             }
         });
+
+        mGoogleSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signInGoogle();
+            }
+        });
+
+        // Configure Google Sign In
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
 
     }
 
@@ -134,4 +155,53 @@ public class LogInActivity extends AppCompatActivity {
                 });
     }
 
+    private void signInGoogle() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                firebaseAuthWithGoogle(account);
+            } catch (ApiException e) {
+                // Google Sign In failed, update UI appropriately
+                Log.w(TAG, "Google sign in failed", e);
+                // ...
+            }
+        }
+    }
+
+    private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
+        Log.d(TAG, "firebaseAuthWithGoogle:" + acct.getId());
+
+        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithCredential:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithCredential:failure", task.getException());
+//                            Snackbar.make(findViewById(R.id.main_layout), "Authentication Failed.", Snackbar.LENGTH_SHORT).show();
+                            updateUI(null);
+                        }
+
+                        // ...
+                    }
+                });
+    }
 }
