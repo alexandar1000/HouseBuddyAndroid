4fda9523bbbb45b7137e1113cdc16c7057ffddd2
rriesebos <rriesebos@hotmail.com>
2018-09-26 13:52:58 +0200

    Some small improvements

--- app/src/main/java/allurosi/housebuddy/todolist/AddTaskDialogFragment.java
+++ app/src/main/java/allurosi/housebuddy/todolist/AddTaskDialogFragment.java
@@ -7,7 +7,6 @@ import android.content.DialogInterface;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
@@ -20,8 +19,6 @@ import android.widget.ImageButton;
 import android.widget.Toast;
 
 import allurosi.housebuddy.R;
-
-import static android.view.Window.FEATURE_NO_TITLE;
 
 public class AddTaskDialogFragment extends DialogFragment {
 
