bb35d38d336ab8dbaa9f52f36b6d828087d572a5
a.s.janjanin <a.s.janjanin@student.rug.nl>
2018-10-11 02:30:34 +0200

    Added a dialogue, need the functionality to add btn and pass input to ac

--- app/src/main/java/allurosi/housebuddy/shoppingList/AddShoppingListItemDialogFragment.java
+++ app/src/main/java/allurosi/housebuddy/shoppingList/AddShoppingListItemDialogFragment.java
@@ -1,7 +1,70 @@
 package allurosi.housebuddy.shoppingList;
 
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.DialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+import allurosi.housebuddy.R;
 
 public class AddShoppingListItemDialogFragment extends DialogFragment {
+
+    private EditText mEditName;
+    private EditText mEditInfo;
+    private Button mAddSItemBtn;
+    private Button mCancelAddSItemBtn;
+
+    public AddShoppingListItemDialogFragment() {}
+
+    public static AddShoppingListItemDialogFragment newInstance(String title) {
+        AddShoppingListItemDialogFragment fragment = new AddShoppingListItemDialogFragment();
+        Bundle args = new Bundle();
+        args.putString("title", title);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.new_shopping_list_item, container, false);
+
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        // Get field from view
+        mEditName = (EditText) view.findViewById(R.id.itemNameEdit);
+        mEditInfo = (EditText) view.findViewById(R.id.itemInfoEdit);
+        mAddSItemBtn = (Button) view.findViewById(R.id.addShoppingItemBtn);
+        mCancelAddSItemBtn = (Button) view.findViewById(R.id.cancelAddingShoppingItemBtn);
+        // Fetch arguments from bundle and set title
+
+        mCancelAddSItemBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getDialog().dismiss();
+            }
+        });
+        String title = getArguments().getString("title", "Enter Name");
+        getDialog().setTitle(title);
+
+
+
+
+
+//         Show soft keyboard automatically and request focus to field
+//        mEditText.requestFocus();
+//        getDialog().getWindow().setSoftInputMode(
+//                WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE);
+    }
 
 }
--- app/src/main/java/allurosi/housebuddy/shoppingList/ShoppingListActivity.java
+++ app/src/main/java/allurosi/housebuddy/shoppingList/ShoppingListActivity.java
@@ -1,23 +1,40 @@
 package allurosi.housebuddy.shoppingList;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import java.util.ArrayList;
 
 import allurosi.housebuddy.R;
 
 public class ShoppingListActivity extends AppCompatActivity {
+
+    private ArrayList<ShoppingItem> shoppingItems = new ArrayList<>();
+
+    private FloatingActionButton mNewItemBtn;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.shopping_list);
+
+        mNewItemBtn = findViewById(R.id.add_shopping_list_item);
+
+        mNewItemBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addNewItem();
+            }
+        });
     }
 
-    public void buttonAddItem() {
-
-//    public void buttonToDoList(View view) {
-        Intent intent = new Intent(this, AddShoppingListItemDialogFragment.class);
-        startActivity(intent);
+    public void addNewItem() {
+        FragmentManager fm = getSupportFragmentManager();
+        AddShoppingListItemDialogFragment addShoppingListItemDialogFragment = AddShoppingListItemDialogFragment.newInstance("Add Item");
+        addShoppingListItemDialogFragment.show(fm, "new_shopping_list_item");
     }
 }
