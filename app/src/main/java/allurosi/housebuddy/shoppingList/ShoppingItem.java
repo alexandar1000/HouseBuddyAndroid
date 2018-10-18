dc42e0fbf5d419e2d1a0ea288e06c7eb3499250c
a.s.janjanin <a.s.janjanin@student.rug.nl>
2018-10-18 04:18:19 +0200

    Updated shoppingItem

--- app/src/main/java/allurosi/housebuddy/shoppingList/ShoppingItem.java
+++ app/src/main/java/allurosi/housebuddy/shoppingList/ShoppingItem.java
@@ -3,10 +3,18 @@ package allurosi.housebuddy.shoppingList;
 public class ShoppingItem {
     private String name;
     private String info;
+    private boolean done;
 
     public ShoppingItem(String name, String info) {
         this.name = name;
         this.info = info;
+        this.done = true;
+    }
+
+    public ShoppingItem(String name, String info, boolean done) {
+        this.name = name;
+        this.info = info;
+        this.done = done;
     }
 
     public String getName() {
@@ -23,5 +31,13 @@ public class ShoppingItem {
 
     public void setInfo(String info) {
         this.info = info;
+    }
+
+    public void setDone(boolean done) {
+        this.done = done;
+    }
+
+    public boolean isDone() {
+        return done;
     }
 }
